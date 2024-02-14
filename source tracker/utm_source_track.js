9ebb30abce42b86049750140ad00026bd4415539
Aayush Maurya <aayushmaurya1099@gmail.com>
2024-02-15 01:34:58 +0530

    included in anonymus function

--- source tracker/utm_source_track.js
+++ source tracker/utm_source_track.js
@@ -1,26 +1,32 @@
-    function addQueryParameters() {
-  // Get the current URL
-  var currentUrl = window.location.href;
-  
-  // Check if the URL already has a query string
-  var hasQueryParams = currentUrl.indexOf('?') !== -1;
+(function() {
+  // Define the addQueryParameters function
+  function addQueryParameters() {
+    // Get the current URL
+    var currentUrl = window.location.href;
+    
+    // Check if the URL already has a query string
+    var hasQueryParams = currentUrl.indexOf('?') !== -1;
 
-  // If there are existing query parameters in the URL, add them to local storage
-  if (hasQueryParams) {
-    var queryParams = currentUrl.split('?')[1];
-    localStorage.setItem('queryParams', queryParams);
-  } else {
-    // If there are no query parameters in the URL, retrieve them from local storage
-    var storedParams = localStorage.getItem('queryParams');
-    if (storedParams) {
-      // Construct the new URL with the stored query parameters
-      var newUrl = currentUrl + '?' + storedParams;
-      
-      // Replace the current URL in the browser history with the new URL
-      window.history.replaceState({}, document.title, newUrl);
+    // If there are existing query parameters in the URL, add them to local storage
+    if (hasQueryParams) {
+      var queryParams = currentUrl.split('?')[1];
+      localStorage.setItem('queryParams', queryParams);
+    } else {
+      // If there are no query parameters in the URL, retrieve them from local storage
+      var storedParams = localStorage.getItem('queryParams');
+      if (storedParams) {
+        // Construct the new URL with the stored query parameters
+        var newUrl = currentUrl + '?' + storedParams;
+        
+        // Replace the current URL in the browser history with the new URL
+        window.history.replaceState({}, document.title, newUrl);
+      }
     }
   }
-}
-// calling function
-console.log("calling addQueryParameters function");
-addQueryParameters();
+
+  // Call the addQueryParameters function when the DOM is ready
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log("calling addQueryParameters function");
+    addQueryParameters();
+  });
+})();
